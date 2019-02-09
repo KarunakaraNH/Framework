f9e0e5985c7dfa1ad088c14bdbe4673159ab804b
karunakaraNH <karunakarakbc@gmail.com>
2019-02-09 07:36:13 +0530

    code change

--- TestNG/src/test/day1.java
+++ TestNG/src/test/day1.java
@@ -4,10 +4,11 @@ import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class day1 {
-   
+   //tis is sample change
 	@BeforeTest
 	public void demo1(){
 		System.out.println("I will exicute 1st");
+		//you can get it 
 	}
 	@Test(groups={"smoke"})
 	public void demo2(){
