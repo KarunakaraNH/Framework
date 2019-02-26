13e7d8d2cacbaa42d147fcc77cdebe869d2c4b0b
karunakaraNH <karunakarakbc@gmail.com>
2019-02-26 15:32:18 +0530

    Code Changes

---
+++ TestNG/src/test/Extent.java
@@ -0,0 +1,11 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class Extent {
+@Test
+	public void Reporter()
+	{
+		Extent
+	}
+}
--- TestNG/src/test/Propertyclass.java
+++ TestNG/src/test/Propertyclass.java
@@ -5,6 +5,7 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -16,7 +17,7 @@ public class Propertyclass {
 	public void prop1() throws IOException{
 		
 		Properties prop=new Properties();//this code is ready to scan the file
-		FileInputStream fis=new FileInputStream("C:\\Users\\karunh\\SeleniumTutorial\\TestNG\\src\\test\\Property.properties");
+		FileInputStream fis=new FileInputStream("C:\\Users\\karunh\\git\\Framework\\TestNG\\src\\test\\Property.properties");
 		//search for that specific file with FIleInputStream java method	
 		prop.load(fis);//.load will exicute the code and it will pickup the value from the property file
 		System.out.println(prop.getProperty("username"));//to get the value of the username
@@ -35,6 +36,9 @@ public class Propertyclass {
 		}
 		
 		driver.get(prop.getProperty("url"));
+		driver.findElement(By.id("nav-link-accountList")).click();
+		driver.findElement(By.xpath("//input[@id='ap_email']")).sendKeys("amazerrr");
+		driver.findElement(By.cssSelector("ap_password")).sendKeys("pass1234");
 	}
 	
 }
